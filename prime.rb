c4d0f6de7a8bb91883cf569e407eb9419d671a48
Aleksandr Roslyakov <torpid@gmail.com>
2018-03-27 16:12:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  end
+  (2...int).each do |divisor|
+    if int % divisor == 0
+      return false
+    end
+  end
+  return true
+end
